7dbdcfd16f14b9655c830f22a64d852f06b67348
DESKTOP-JP1MEGQ\Wang <brzden32@gmail.com>
2021-05-15 09:05:10 -0700

    unsafe merge again

--- src/@fuse/core/FuseNavigation/FuseNavItem.js
+++ src/@fuse/core/FuseNavigation/FuseNavItem.js
@@ -3,9 +3,9 @@ import React from 'react';
 const components = {};
 
 export function registerComponent(name, Component) {
-	const c = Component;
+	const n = name;
 	Object.freeze(components);
-	components[name] = c;
+	components[n] = Component;
 }
 
 export default function FuseNavItem(props) {
